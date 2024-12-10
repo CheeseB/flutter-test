bd46995d2dddaa843d7333a30a514a2af60384da
seongyeon.kim <seongyeon.kim@gintlab.com>
2024-12-10 16:53:37 +0900

    등간격 회전 기능 삭제

--- lib/map_sample.dart
+++ lib/map_sample.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'utils.dart';
 
 class MapSample extends StatefulWidget {
   const MapSample({super.key});
@@ -15,12 +14,12 @@ class MapSampleState extends State<MapSample> {
       Completer<GoogleMapController>();
   static const CameraPosition _initialCameraPosition = CameraPosition(
     target: LatLng(38.097418, 127.072470),
-    zoom: 19.5,
+    zoom: 18,
   );
 
   final Set<Marker> _markers = {};
   final Set<Polyline> _polylines = {};
-  double _rotationAngle = 0.0;
+  final double _rotationAngle = 0.0;
 
   @override
   void initState() {
@@ -77,35 +76,27 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onScaleUpdate: (details) {
-          setState(() {
-            _rotationAngle = details.rotation;
-            _updatePolylines();
-          });
-        },
-        child: Stack(
-          children: [
-            GoogleMap(
-              mapType: MapType.satellite,
-              initialCameraPosition: _initialCameraPosition,
-              markers: _markers,
-              polylines: _polylines,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-              myLocationButtonEnabled: false,
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.satellite,
+            initialCameraPosition: _initialCameraPosition,
+            markers: _markers,
+            polylines: _polylines,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            myLocationButtonEnabled: false,
+          ),
+          Positioned(
+            bottom: 50,
+            right: 10,
+            child: FloatingActionButton(
+              onPressed: _addAMarker,
+              child: const Text('A'),
             ),
-            Positioned(
-              bottom: 50,
-              right: 10,
-              child: FloatingActionButton(
-                onPressed: _addAMarker,
-                child: const Text('A'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -121,38 +112,5 @@ class MapSampleState extends State<MapSample> {
         ),
       );
     });
-  }
-
-  void _updatePolylines() {
-    const double gap = 0.0001;
-    final double centerLat = _initialCameraPosition.target.latitude;
-    final double centerLng = _initialCameraPosition.target.longitude;
-
-    _polylines.clear();
-    _polylines.addAll(
-      List.generate(5, (index) {
-        int i = index - 2;
-        final double offsetLng = i * gap;
-
-        final LatLng start = rotatePoint(
-          LatLng(centerLat - 0.01, centerLng + offsetLng),
-          LatLng(centerLat, centerLng),
-          _rotationAngle,
-        );
-
-        final LatLng end = rotatePoint(
-          LatLng(centerLat + 0.01, centerLng + offsetLng),
-          LatLng(centerLat, centerLng),
-          _rotationAngle,
-        );
-
-        return Polyline(
-          polylineId: PolylineId('line_$i'),
-          color: i == 0 ? Colors.red : Colors.white,
-          width: 2,
-          points: [start, end],
-        );
-      }),
-    );
   }
 }
